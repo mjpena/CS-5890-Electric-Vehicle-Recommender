b57b088d2b19abe4eff22f13315f1d8a43ed15e8
Melanie Pena <penamelanie3@gmail.com>
2018-12-02 21:45:12 -0700

    fixed state dictionary

--- reward.py
+++ reward.py
@@ -1,19 +1,20 @@
 # peak schedule: https://www.rockymountainpower.net/ya/po/otou.html
 # prices per kwh: https://www.rockymountainpower.net/content/dam/rocky_mountain_power/doc/About_Us/Rates_and_Regulation/Utah/Approved_Tariffs/Rate_Schedules/Residential_Service_Optional_Time_of_Day_Rider_Experimental.pdf
 # ontario data: https://www.powerstream.ca/customers/rates-support-programs/time-of-use-pricing.html
-# nissan leaf specs: https://www.evbox.com/electric-cars/nissan/nissan-leaf-2018
+# charging data: https://pushevs.com/2018/05/21/fast-charging-curves/
 
-#need eBatt_max value
-eBatt_max = 2
+# battery size and max charging rate of a nissan leaf
+eBatt_capacity = 40
+max_delta_e = 40
 
-# dictionary holding all possible combinations of battery charge at an hour
-"""V = []
+# dictionary holding all possible states
+V = {}
 for time in range(0, 2):
         V[time] = V.get(time, {})
-        for eBatt in range(0, eBatt_max):
-                V[time][eBatt] =  0
-                print(V[time, eBatt])
-   """ 
+        for eBatt in range(0, eBatt_capacity):
+                V[time][eBatt] = 0
+                # print(V[time][eBatt])
+
 
 # takes in current state and change in eBatt; gives you next state
 def action(eBatt, time, delta_e):
@@ -25,13 +26,16 @@ def action(eBatt, time, delta_e):
 
 # returns 
 def reward(state, action):
-        eBatt_capacity = 40
         curr_eBatt, curr_time = state
         next_eBatt, next_time = action
 
         # no charging occurs, no reward
         if(curr_eBatt >= next_eBatt):
                 return 0
+
+        #if overcharging occur, negative reward
+        if(next_eBatt > eBatt_capacity):
+                return -100
 
         # day is split up into 72(24*3) sections or 20 minute intervals so divide back into just hours
         curr_hours = curr_time / 3
@@ -46,25 +50,19 @@ def reward(state, action):
                 else:
                                 price = price + 0.016334
                 
-        #if overcharging occurs
-        if(next_eBatt > eBatt_capacity):
-                return -100
 
 
-        
 
-
-# possible reward for making it through the day
-"""def V(eBatt, time, delta_e):
-        EV_batt_max = 0
+# fills state dictionary with associated reward 
+def value_function(eBatt, time, delta_e):
         max_error = 0
         for time in range(0, 24*3):
-            for eBatt in range(0, EV_batt_max):
-                    V = V[(eBatt, time)]
-                    best = -10000000000
+            for eBatt in range(0, eBatt_capacity):
+                    value = V[eBatt][time]
+                    best = -100
                     for delta_e in range(0, max_delta_e + 1):
-                            best = max(best, reward(eBatt, time, delta_e)+ V[action(eBatt, time, delta_e)])
-                            max_error = max(max_error, abs(V-best))
+                            v_eBatt, v_time = action(eBatt, time, delta_e)
+                            best = max(best, reward((eBatt, time), delta_e)+ V[v_eBatt][v_time])
+                            max_error = max(max_error, abs(value-best))
 
-"""
 
