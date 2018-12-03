71e2416148d5fccabfe10b64fbdbdd90d5eecc21
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2018-12-02 23:12:58 -0700

    started debugging

--- reward.py
+++ reward.py
@@ -2,18 +2,22 @@
 # prices per kwh: https://www.rockymountainpower.net/content/dam/rocky_mountain_power/doc/About_Us/Rates_and_Regulation/Utah/Approved_Tariffs/Rate_Schedules/Residential_Service_Optional_Time_of_Day_Rider_Experimental.pdf
 # ontario data: https://www.powerstream.ca/customers/rates-support-programs/time-of-use-pricing.html
 # charging data: https://pushevs.com/2018/05/21/fast-charging-curves/
-
+import math
 # battery size and max charging rate of a nissan leaf
 eBatt_capacity = 40
 max_delta_e = 40
 
 # dictionary holding all possible states
+V = [[40],[72]]
+
+'''
 V = {}
 for time in range(0, 2):
-        V[time] = V.get(time, {})
+        V[time][] = V.get(time, )
         for eBatt in range(0, eBatt_capacity):
                 V[time][eBatt] = 0
                 # print(V[time][eBatt])
+'''
 
 
 # takes in current state and change in eBatt; gives you next state
@@ -27,7 +31,7 @@ def action(eBatt, time, delta_e):
 # returns reward at given state and action 
 def reward(state, action):
         curr_eBatt, curr_time = state
-        next_eBatt, next_time = action
+        next_eBatt= action
 
         #if overcharging occur, negative reward
         if(next_eBatt > eBatt_capacity):
@@ -40,9 +44,9 @@ def reward(state, action):
 
         # day is split up into 72(24*3) sections or 20 minute intervals so divide back into just hours
         curr_hours = curr_time / 3
-        next_hours = next_time / 3
+        next_hours = (curr_time+1) / 3
 
-        for time in range(curr_hours, next_hours):
+        for time in range(math.floor(curr_hours), math.floor(next_hours)):
                 off_peak_price = 0.016334
                 on_peak_price = 0.043560
                 best_price = abs(curr_hours-next_hours) * off_peak_price
@@ -74,4 +78,4 @@ def value_function(eBatt, time, delta_e):
                             best = max(best, reward((eBatt, time), delta_e)+ V[v_eBatt][v_time])
                             max_error = max(max_error, abs(value-best))
 
-
+value_function(1,1,1)      
