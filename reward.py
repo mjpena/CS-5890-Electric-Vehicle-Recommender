934f45bac868ce7cd4385eeac9d58378cb7afa63
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2018-12-13 12:07:55 -0700

    update to graph

--- reward.py
+++ reward.py
@@ -5,13 +5,16 @@
 
 import math
 import data1
-
+import matplotlib.pyplot as plt
 # battery capacity in percentages
-EBATT_CAPACITY = 24
+EBATT_CAPACITY = 25
 # how much to multiply 24 hours by to get number of sections a day is split up into
 TIME_FACTOR = 3
 # number of sections a day is split up into
-TIME_SECTIONS = TIME_FACTOR * 24
+#TIME_SECTIONS = TIME_FACTOR * 24
+dayCh=data1.storecharge()   
+NIGHT_TIME_SECTIONS=(72-data1.findPrevTime(data1.storePrevCharge()))
+TIME_SECTIONS =data1.findTime(data1.storecharge())+NIGHT_TIME_SECTIONS
 level1=4
 level2=22
 start_time = 1
@@ -19,9 +22,9 @@ start_time = 1
 
 # returns 
 def max_delta_e(fullnes):
-       if(fullnes/EBATT_CAPACITY < 60):
+       if(((fullnes*1.0))/(EBATT_CAPACITY*1.0) < .60):
                return math.floor((level1*2)/TIME_FACTOR)
-       return math.floor(level1/TIME_FACTOR)
+       return math.floor((level1*1.0)/TIME_FACTOR)
 
 
 # dictionary holding all possible states
@@ -38,29 +41,18 @@ def action(eBatt, time, delta_e):
         return s_prime
 
 # 
-def calc(eBatt) :
-        x=0
-        #print (eBatt-trip[0])
-        if(eBatt-trip[0]<=0):
-                x = -300
-        return x
 
-# 
-def termReward(goTime):
-        for eBatt in range(0,EBATT_CAPACITY):
-                for time in range(goTime,TIME_SECTIONS):
-                        x=calc(eBatt)
-                        V[eBatt][time]=x
         #print(V)
 
 
 # returns cost of energy at given time of day
-tod_price = [0]*24
+tod_price = [0]*72
 for i in range(0, len(tod_price)):
-        if(i >= 13 * TIME_FACTOR and i<= 20 * TIME_FACTOR):
+        if(i >= 13 * TIME_FACTOR and i<=20*TIME_FACTOR): #20 * TIME_FACTOR):
                 tod_price[i] = 0.043560
         else:
                 tod_price[i] = 0.016334
+
 
 
 # returns reward at given state and action 
@@ -68,14 +60,15 @@ def reward(state, action):
         
         eBatt, time = state
         delta_e = action
-       # print(dayCh[time])
-        if(dayCh[time]==0.0):
-                #print('*****************************')
-                return 0
-        
-        # start_hour being when the entire trip starts?
+        if (time<=NIGHT_TIME_SECTIONS):
+                time=time+(72-NIGHT_TIME_SECTIONS)
+                #print("n")
+        else:
+                #print("d")
+                time = time-NIGHT_TIME_SECTIONS
         hour = int(start_time + (time/3))
         price = tod_price[hour % 24] * delta_e
+        price-=.001
         return price
 
          
@@ -86,38 +79,64 @@ def value_function():
         while(max_error):
                 max_error = 0
                 #terminal
-                for eBatt in range(0, EBATT_CAPACITY):
-                        for time in range(0, TIME_SECTIONS - 1):
+                
+                for time in range(0, TIME_SECTIONS - 1):
+                        for eBatt in range(0, EBATT_CAPACITY-1):
                                 value = V[eBatt][time]
                                 #print (value)
-                                best = -100
+                                best = 100
                                 
-                                for delta_e in range(0, max_delta_e(value) + 1):
+                                for delta_e in range(0, max_delta_e(eBatt) + 1):
                                         v_eBatt, v_time = action(eBatt, time, delta_e)
-                                        if(v_eBatt > EBATT_CAPACITY - 1):
-                                                break
-                                        if(v_time > TIME_SECTIONS - 1):
-                                                break
+                                        #if(v_eBatt > EBATT_CAPACITY - 1):
+                                         #       break
+                                        #if(v_time > TIME_SECTIONS - 1):
+                                         #       break
                                         
-                                        best = max(best, reward((eBatt, time), delta_e)+ V[v_eBatt][v_time])
+                                        best = min(best, reward((eBatt, time), delta_e)+V[v_eBatt][v_time])
                                         
-                                         #abs(best)
-                                       # best = abs(best)
-                                #print (value)
+                                       # eBatt=v_eBatt 
+                               # print (best)
+                                V[eBatt][time]=best              
                                 max_error =  max(max_error, abs(value-best))
-                                V[eBatt][time]=best
-                               # print(max_error)
+                               # print (max_error)
+                                
                 
+def calc(eBatt) :
+        x=0
+        if(eBatt-trip[0]<=15):
+                x = .25
+        return x
 
+# 
+def termReward(goTime,hTime):
+        for eBatt in range(0,EBATT_CAPACITY):
+                        x=calc(eBatt)
+                        V[eBatt][TIME_SECTIONS-1]=x
+       # print (V)
 #home =data1.storecharge()
 #print(home)
 trip=data1.storeUse()
 home = 18
-dayCh=data1.storecharge()   
 
-termReward(18)
-#print(V)
-value_function()   
-#print(V)
-#V[1][1]= action(1,1,1))   
-#print(V)
+
+termReward(18,18*3) 
+value_function() 
+
+print(V[10])
+
+plt.imshow(V, interpolation="nearest",origin='lower')
+
+plt.colorbar()
+plt.ylabel('battery charged')
+plt.xlabel('Time in twenty minute intervals'+str(NIGHT_TIME_SECTIONS)+' is midnight')
+plt.show()
+'''
+plt.bar(V,V[], align='center')
+plt.show
+
+    #plt.xticks(y_pos, objects)
+plt.title('Estimated possible charging time per day')
+
+plt.show()
+'''
