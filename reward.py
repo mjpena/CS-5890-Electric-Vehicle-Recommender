aea7ec5cd92a67ec5bd29bf0d0c339a518706312
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2018-12-13 13:47:32 -0700

    upToDate!!!!!!!!!!!!!

--- reward.py
+++ reward.py
@@ -14,6 +14,7 @@ TIME_FACTOR = 3
 #TIME_SECTIONS = TIME_FACTOR * 24
 dayCh=data1.storecharge()   
 NIGHT_TIME_SECTIONS=(72-data1.findPrevTime(data1.storePrevCharge()))
+#print(data1.findTime(data1.storecharge()))
 TIME_SECTIONS =data1.findTime(data1.storecharge())+NIGHT_TIME_SECTIONS
 level1=4
 level2=22
@@ -48,11 +49,10 @@ def action(eBatt, time, delta_e):
 # returns cost of energy at given time of day
 tod_price = [0]*72
 for i in range(0, len(tod_price)):
-        if(i >= 13 * TIME_FACTOR and i<=20*TIME_FACTOR): #20 * TIME_FACTOR):
-                tod_price[i] = 0.043560
+        if(i >= 23 * TIME_FACTOR or i<=4*TIME_FACTOR): #20 * TIME_FACTOR):
+                tod_price[i] = 0.016334 
         else:
-                tod_price[i] = 0.016334
-
+                tod_price[i] = 0.043560
 
 
 # returns reward at given state and action 
@@ -60,15 +60,16 @@ def reward(state, action):
         
         eBatt, time = state
         delta_e = action
-        if (time<=NIGHT_TIME_SECTIONS):
-                time=time+(72-NIGHT_TIME_SECTIONS)
-                #print("n")
+        timeOfDay=(time+(72-NIGHT_TIME_SECTIONS))%72
+
+        #hour = int(start_time + (time/3))
+        price = tod_price[timeOfDay] * delta_e
+        #price-=.001
+        if action < EBATT_CAPACITY /20.0:
+                rBatt = 0
         else:
-                #print("d")
-                time = time-NIGHT_TIME_SECTIONS
-        hour = int(start_time + (time/3))
-        price = tod_price[hour % 24] * delta_e
-        return price
+                rBatt = 0.5*action/(EBATT_CAPACITY*1.0)
+        return price + rBatt
 
          
 
@@ -77,34 +78,38 @@ def value_function():
         max_error = 2
         while(max_error):
                 max_error = 0
-                #terminal
-                
                 for time in range(0, TIME_SECTIONS - 1):
                         for eBatt in range(0, EBATT_CAPACITY-1):
                                 value = V[eBatt][time]
-                                #print (value)
                                 best = 100
-                                
                                 for delta_e in range(0, max_delta_e(eBatt) + 1):
                                         v_eBatt, v_time = action(eBatt, time, delta_e)
-                                        #if(v_eBatt > EBATT_CAPACITY - 1):
-                                         #       break
-                                        #if(v_time > TIME_SECTIONS - 1):
-                                         #       break
-                                        
                                         best = min(best, reward((eBatt, time), delta_e)+V[v_eBatt][v_time])
-                                        
-                                       # eBatt=v_eBatt 
-                               # print (best)
                                 V[eBatt][time]=best              
                                 max_error =  max(max_error, abs(value-best))
-                               # print (max_error)
+                               
+                               
                                 
-                
+def policy(eBatt): #beginning battery at index 0
+    # return the policy for each index
+    policy = []
+    time = 0
+    for time in range(0, TIME_SECTIONS - 1):
+        best = 100000
+        bestDeltaE=0
+        for delta_e in range(0, max_delta_e(eBatt) + 1):
+           v_eBatt, v_time = action(eBatt, time, delta_e)
+           now = reward((eBatt, time), delta_e)+V[v_eBatt][v_time]
+           if now <= best:
+               best = now
+               bestDeltaE = delta_e
+        policy.append(bestDeltaE)
+    return policy
+        
 def calc(eBatt) :
         x=0
-        if(eBatt-trip[0]<=15):
-                x = .25
+        if(eBatt-trip[0]<=12):
+                x = .55
         return x
 
 # 
@@ -116,19 +121,25 @@ def termReward(goTime,hTime):
 #home =data1.storecharge()
 #print(home)
 trip=data1.storeUse()
-home = 18
-
+#home = 18
+#print(trip[0]+10)
 
 termReward(18,18*3) 
 value_function() 
 
-print(V[10])
+#print(V[10])
 
 plt.imshow(V, interpolation="nearest",origin='lower')
 
 plt.colorbar()
-plt.ylabel('battery charged')
+plt.ylabel('battery charged kwh')
 plt.xlabel('Time in twenty minute intervals'+str(NIGHT_TIME_SECTIONS)+' is midnight')
+ax = plt.axes()
+eBatt = 5
+policy = policy(eBatt)
+for i in range(0, TIME_SECTIONS - 1):
+    ax.arrow(i, eBatt , 1, policy[i], head_width=0.05, head_length=0.1, fc='k', ec='k')
+    eBatt = eBatt + policy[i]
 plt.show()
 '''
 plt.bar(V,V[], align='center')
